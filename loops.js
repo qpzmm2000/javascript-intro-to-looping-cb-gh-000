f79e919bf2f3361187a87f716bad2ae668546764
nick Wan <kwoklwan3-c@my.cityu.edu.hk>
2017-09-28 07:10:02 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,3 +4,10 @@ function forLoop(array){
     array.push(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
   }
 }
+function whileLoop(num)
+{
+  while (num > 0) {
+    console.log(--num)
+  }
+}
+
